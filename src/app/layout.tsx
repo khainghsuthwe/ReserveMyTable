a49f90a8495537310b1d8ef901cd42c859279c52
khainghsuthwe <khaingjar@gmail.com>
2025-05-08 13:29:04 +0630

    updated site SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,39 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { motion } from 'framer-motion';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'ReserveMyTable',
+    template: '%s | ReserveMyTable',
+  },
+  description: 'Welcome to ReserveMyTable — restaurant booking made easy.',
+  openGraph: {
+    title: 'MySite',
+    description: 'Welcome to ReserveMyTable — restaurant booking made easy.',
+    url: 'https://restaurant-booking-frontend-rho.vercel.app/',
+    siteName: 'ReserveMyTable',
+    images: [
+      {
+        url: 'https://www.example.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'MySite',
+    description: 'Welcome to ReserveMyTable — restaurant booking made easy.',
+    images: ['https://www.example.com/twitter-image.jpg'],
+    creator: '@yourhandle',
+  },
+  metadataBase: new URL('https://restaurant-booking-frontend-rho.vercel.app/'),
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
